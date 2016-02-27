cacd13c5b042a1fb700a013ae1eb356c73e63ccc
jonathan <jflukkien@gmail.com>
2016-02-27 15:58:42 +0100

    Last commit of the python project

--- server.py
+++ server.py
@@ -1,63 +1,62 @@
+#! env/bin/python
 import json
-from flask import Flask, render_template, request, redirect, url_for, abort, session
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 users_file = 'data/addressbook.json'
 act_file = 'data/activities.json'
+private_key=''
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def home_page():
+    return flask.render_template("index.html")
 
 @app.route('/leden/')
 def members_page():
     jdata = load_json_by(users_file)
     print('found users')
-    return render_template('user_list.html', users=jdata)
+    return flask.render_template('user_list.html', users=jdata)
 
 @app.route('/leden/<username>')
 def get_member_by(username):
-    error = None
     juser = find_json_item_by(username, load_json_by(users_file))
-    if juser is None:
-        error = 'Gebruiker kon niet gevonden worden'
-    return render_template('user_page.html', user=juser, error=error)
+    return flask.render_template('user_page.html', user=juser)
 
 @app.route('/activiteiten/')
 def activity_page():
-    return render_template('activities.html', activities=load_json_by(act_file))
+    return flask.render_template('activities.html', activities=load_json_by(act_file))
 
 @app.route('/activiteiten/<activity_id>')
 def get_activity_by(activity_id):
-    error = None
     jactivity = find_json_item_by(activity_id, load_json_by(act_file))
-    if jactivity is None:
-        error = 'Activiteit kon niet gevonden worden'
-    return render_template('activity.html', activity=jactivity, error=error)
+    return flask.render_template('activity.html', activity=jactivity)
 
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST']) 
 def login():
-    error = None
     if request.method == 'POST':
         juser = find_json_item_by(request.form.username, load_json_by(users_file))
-        if match_pashword(juser, request.form.pashword):
-            return redirect('/')
-        error = 'invalid credentials'
-    return render_template('login.html', error)
-        
-def match_pashword(juser, pashword):
-    return juser['pashword'] == pashword
+        if verify_password(juser, request.form.password)
+           return redirect('/') 
+    return flask.render_template('login.html')
+
+def verify_password(json_user, password):
     
+
+
 def load_json_by(json_file_name):
     with open(json_file_name) as data_file:
         return json.loads(data_file.read())
-    return None
+    flask.abort(404)
         
 def find_json_item_by(json_key, jdata):
     for jkey in jdata:
         if jkey == json_key:
             return jdata[jkey]
-    return None
+    flask.abort(404)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template(error.html)
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
